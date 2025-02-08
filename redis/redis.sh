51d925b9eae6a2b2a652c7b071bf9ac38f21503a
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-02-08 20:06:22 +0900

    List자료구조 추가

--- redis/redis.sh
+++ redis/redis.sh
@@ -33,3 +33,32 @@ decr stocks:product:1
 
 # redis활용 : 캐시 기능 구현
 set posting:1 "{ \"title\":\"hello java\", \"contents\":\"hello java is...\", ...}" ex 100
+
+# List자료구조 (deque를 가짐)
+# list 삽입, 제거
+lpush hongildongs hong1
+lpush hongildongs hong2
+rpush hongildongs hong3
+lrange hongildons 0 -1 (# 결과: hong2, hong1, hong3)
+rpop hongildongs
+lpop hongildongs
+# list 조회
+# -1 은 리스트의 끝자리(마지막 index)를 의미. -2는 끝에서 2번째를 의미
+lrange hongildongs 0 0 #첫번째 값만 조회
+lrange hongildongs -1 -1 #마지막 값만 조회
+lrange hongildongs 0 -1
+lrange hongildongs -2 -1 #마지막 2번째부터 마지막자리까지
+lrange hongildongs 0 1 #첫 번쨰부터 2번쨰까지
+# 데이터 개수 조회
+llen hongildongs
+# TTL 적용
+EXPIRE hongildongs 20
+# TTL 조회
+ttl hongildongs
+# redis 활용 : 최근 방문한 페이지, 최근 조회한 상품목록
+rpush mypages www.naver.com
+rpush mypages www.google.com
+rpush mypages www.chatgpt.com
+rpush mypages www.naver.com
+#최근 방문한 페이지 3개만 보여주는
+lrange mypages -3 -1
